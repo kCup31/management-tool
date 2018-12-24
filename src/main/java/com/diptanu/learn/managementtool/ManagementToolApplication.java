d0bfeb7b5f0443e82d6404db7d4b7c86b5164b74
Das <dasd5@usxxdasd5m2.corp.emc.com>
2018-12-24 11:28:42 -0500

    first commit

---
+++ src/main/java/com/diptanu/learn/managementtool/ManagementToolApplication.java
@@ -0,0 +1,14 @@
+package com.diptanu.learn.managementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementToolApplication.class, args);
+    }
+
+}
+
