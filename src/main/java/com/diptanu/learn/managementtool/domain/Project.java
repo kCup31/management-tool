b7f57bfff49b9929b012fd7305ef5c1e1948bf40
Das <dasd5@usxxdasd5m2.corp.emc.com>
2018-12-24 18:22:56 -0500

    added object validation

--- src/main/java/com/diptanu/learn/managementtool/domain/Project.java
+++ src/main/java/com/diptanu/learn/managementtool/domain/Project.java
@@ -1,14 +1,18 @@
 package com.diptanu.learn.managementtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @NoArgsConstructor
@@ -20,18 +24,27 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project Name is required")
     private String projectName;
 
+    @NotBlank(message = "ProjectIdentifier is required")
+    @Size(min = 4, max = 5, message = " Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
 
+    @NotBlank(message = "Project description is required")
     private String description;
 
+    @JsonFormat(pattern = "YYYY-mm-dd")
     private Date startDate;
 
+    @JsonFormat(pattern = "YYYY-mm-dd")
     private Date endDate;
 
+    @JsonFormat(pattern = "YYYY-mm-dd")
     private Date created_at;
 
+    @JsonFormat(pattern = "YYYY-mm-dd")
     private Date updated_at;
 
     @PrePersist
@@ -43,5 +56,4 @@ public class Project {
     protected void onUpdate() {
         this.updated_at = new Date();
     }
-
 }
